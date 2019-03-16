f2a84a79f16703ecbec35589402a1c0c579d2a42
janaravi10 <janaravi962@gmail.com>
2019-03-16 22:57:40 +0530

    Added content to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-import { Row } from "reactstrap";
+import { Row, Col } from "reactstrap";
 export default class Footer extends Component {
   render() {
     return (
-      <footer
-        className="container-fluid bg-dark"
-        style={{ height: "80px" }}
-      >
-        <Row>&copy; copyright @ NEWS 24/7</Row>
+      <footer className="container-fluid bg-dark">
+        <Row className="p-3 text-white d-flex">
+          <Col className="col-lg-4 col-12">
+            &copy; copyright @ NEWS 24/7
+          </Col>
+          <Col className="col-lg-4 col-12">
+            <b> Technology stack used</b>
+            <span> React , Redux , Reactstrap , Bootstrap</span>
+          </Col>
+          <Col className="col-lg-4 col-12">
+            <b>
+              Thanks to <a href="https://newsapi.org/"> newsapi.org </a>
+              for their awesome api
+            </b>
+          </Col>
+        </Row>
       </footer>
     );
   }
