12abf3689ad4c29ba7343257483ab05551a60bef
janaravi10 <janaravi962@gmail.com>
2019-03-16 22:58:15 +0530

    added the setLanguage action creator

--- src/actions/newsAction.js
+++ src/actions/newsAction.js
@@ -1,26 +1,36 @@
-import { GOT_NEWS, NEWS_LOADING_FAILED } from "./actionTypes";
+import { GOT_NEWS, NEWS_LOADING_FAILED, SETTINGS_UPDATE } from "./actionTypes";
 import axios from "axios";
 export const getNews = () => {
   return dispatch => {
-    axios
-      .get(
-        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=e12c815d5ca54ead874a01c0feb3d58d"
-      )
-      .then(res => {
-        if (res.data.status === "ok") {
-          dispatch({
-            type: GOT_NEWS,
-            payload: { news: res.data.articles, isLoading: false }
-          });
-        } else {
-          dispatch({
-            type: NEWS_LOADING_FAILED,
-            payload: {
-              isLoading: false,
-              error: "Something went wrong :("
-            }
-          });
-        }
-      });
+    axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=e12c815d5ca54ead874a01c0feb3d58d").then(res => {
+      if (res.data.status === "ok") {
+        dispatch({
+          type: GOT_NEWS,
+          payload: { news: res.data.articles, isLoading: false }
+        });
+      } else {
+        dispatch({
+          type: NEWS_LOADING_FAILED,
+          payload: {
+            isLoading: false,
+            error: "Something went wrong :("
+          }
+        });
+      }
+    });
+  };
+};
+export const setLanguage = language => {
+  return dispatch => {
+    // set language to the local storage
+    localStorage.setItem("language", language);
+
+    dispatch({
+      type: SETTINGS_UPDATE,
+      payload: {
+        language: language
+      }
+    });
+    getNews()(dispatch);
   };
 };
